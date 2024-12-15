6add1c1e2201fda8b44dfbc5f0521336c6725769
Harshsingh656 <hardbh56@gmail.com>
2024-12-15 11:52:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('forgiveButton').addEventListener('click', function () {
+    alert("Thank you for forgiving me, Mom! 🐶❤️");
+    document.body.style.background = "linear-gradient(to bottom, #a1ffce, #faffd1)";
+});
